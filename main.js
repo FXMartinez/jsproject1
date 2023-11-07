3e45b8f2d95aad5922bd3bb344a846b0f9f2d683
Felix <felixaviermartinez@gmail.com>
2023-11-07 11:39:36 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let body = document.getElementById('body');
+    let test = "<h1> HELLO </h1>";
+    body.innerHTML += test;
+    console.log('does this hit')
+
+});

ffad372f29fcb5167ab112bae688d39c0178a042
Felix <felixaviermartinez@gmail.com>
2023-11-07 12:47:47 -0500

    css structure added

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    let body = document.getElementById('body');
-    let test = "<h1> HELLO </h1>";
-    body.innerHTML += test;
     console.log('does this hit')
 
 });
